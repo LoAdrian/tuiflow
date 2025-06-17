d81aae2d8e334660b3def59786eb23ac8ba824ce
Adrian Locher <adrian.locher@digitecgalaxus.ch>
2025-06-17 23:04:22 +0200

    delete old docs and unused macro for builders

--- buildable-macro/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use buildable::buildable_impl;
-use proc_macro::TokenStream;
-
-mod buildable;
-
-#[proc_macro_attribute]
-pub fn buildable(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    buildable_impl(item)
-}
