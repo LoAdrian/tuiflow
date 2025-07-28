3f1cdbb6f68fc93ac9aff28ad6bca358306a8353
Adrian Locher <adrian.locher@digitecgalaxus.ch>
2025-07-28 22:03:31 +0200

    implement automock for Transit trait

--- tuiflow-model/src/model/state.rs
+++ tuiflow-model/src/model/state.rs
@@ -1,12 +1,14 @@
 mod workflow_state;
 mod state;
 
+use mockall::automock;
 pub use workflow_state::*;
 pub use state::*;
 use tuiflow_model_contracts::control::Control;
 use tuiflow_model_contracts::error::StateTransitionError;
 use crate::model::variable::VariableSet;
 
+#[automock]
 pub trait Transit: Sized {
     fn run(
         &self,
